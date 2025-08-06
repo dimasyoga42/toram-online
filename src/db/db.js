758a07702c25b9f4c3858cdcdcc335677decb3b2
dimasyoga42 <dimasyoga9090@gmail.com>
2025-08-06 14:30:13 +0700

    DELETE

--- src/db/db.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
-export const supabase = createClient(
-	"https://dpzomyklvfwauezwmdja.supabase.co",
-	process.env.APIKEY
-);
